2ffb0df368765a980257da8e674cb1ac77ceff11
Sergio Camalich <sergio@esecamalich.com>
2017-12-13 16:09:54 +0700

    Creación de primeros archivos

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>CB2</title>
+		<link rel="apple-touch-icon" href="images/apple-touch-icon-precomposed.png"/>
+		<link rel="apple-touch-startup-image" href="images/a-300.png">
+		<meta name="apple-mobile-web-app-capable" content="yes">
+		<meta name="apple-mobile-web-app-status-bar-style" content="#008178" />
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+		<link href="favicon.ico" rel="shortcut icon">
+		<link rel="stylesheet" href="css/main.css" />
+		<!--[if lte IE 7]>
+		<link rel="stylesheet" type="text/css" href="css/ie.css" />
+		<![endif]-->
+		<!--[if lt IE 9]>
+		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+	</head>
+	<body>
+		<img class="logo intro-logo svg" src="images/BC2-logo.svg" alt="BC2" title="BC2">
+		<!-- SCRIPTS -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script>
+		if (!window.jQuery) {
+		    document.write('<script src="js/jquery.js"><\/script>');
+		}
+		</script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
+		<!-- /SCRIPTS -->
+	</body>
+</html>
