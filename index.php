24db0167dac0f8673633ad357cc3de7ba400c000
Sergio Camalich <sergio@esecamalich.com>
2017-12-14 09:33:36 +0700

    Nuevas Imagenes

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 	<head>
+		<link href="https://fonts.googleapis.com/css?family=Work+Sans:400,600,700" rel="stylesheet">
 		<meta charset="utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title>CB2</title>
@@ -22,7 +23,56 @@
 		<![endif]-->
 	</head>
 	<body>
-		<img class="logo intro-logo svg" src="images/BC2-logo.svg" alt="BC2" title="BC2">
+		<!-- INTRO -->
+		<section class="block pad-20 container bg-dark txt-white intro">
+			<header class="block-ctr main-header">
+				<a href="index.php" class="left logo-container">
+					<img class="logo intro-logo svg" src="images/BC2-logo.png" alt="BC2" title="BC2">
+				</a>
+				<div class="right icon-container icon-container-menu">
+					<span class="icon icon-menu"></span>
+				</div>
+				<!--<div class="right header-rgt">
+					<nav class="right">
+
+					</nav>
+				</div>-->
+			</header>
+			<div class="clear"></div>
+		</section>
+		<!-- /INTRO -->
+		<!-- ACERCA -->
+		<section class="clear pad-40 neg-50 container acerca">
+			<div class="block-ctr acerca txt-white txt-center">
+				<h1>Acerca de Nosotros</h1>
+				<p class="bg-white pad-20 acerca-txt">
+					BC2 es un espacio para compartir información relevante sobre el mundo de los negocios; principalmente en materia corporativa, tributaria, legal y financiera.
+				</p>
+			</div>
+		</section>
+		<div class="bg-gray full-w pad-40 acerca-callto">
+			<a href="nosotros.php" class="center btn btn-outline btn-blue btn-acerca">
+				Más sobre nosotros
+			</a>
+		</div>
+		<!-- /ACERCA -->
+		<!-- DESTACADAS -->
+		<section class="container bg-white pad-40 publicaciones-destacadas col-3">
+			<h2>Publicaciones Destacadas</h2>
+			<ul class="col-3-list list list-destacadas">
+				<li>
+					<a href="detalle.php">
+						<img src="images/bandera.jpg" alt="Bandera" title="Bandera" />
+					</a>
+					<h1>
+						<a href="detalle.php">
+							Adquisición de bienes raíces por extranjeros en México.
+						</a>
+					</h1>
+				</li>
+			</ul>
+		</section>
+		<!-- /DESTACADAS -->
 		<!-- SCRIPTS -->
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 		<script>
